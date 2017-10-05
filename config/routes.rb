7ffbc87e47625bd3a72a4876a0d6551e5052fdb3
Malachai Frazier <malachaifrazier@users.noreply.github.com>
2017-10-04 23:53:45 -0400

    OCD white spacing

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  # form_for is easier to use with a resourceful route
   resources :contact_forms, only: [:create]
   # A non-resourceful route was used to place the contact form at /contact
   get 'contact' => 'contact_forms#new', as: 'contact'
